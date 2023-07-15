3ce0fd6a9cea25faf9907f575236dd83189caafb
Manju Kran H C <manjukiranhc7@gmail.com>
2023-07-15 23:05:58 +0530

    login authentcation implementation

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User_schema');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
+const secret = 'adafadasgsfgsgsfgsdfa';
 
 const salt = bcrypt.genSaltSync(10);
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://manjukiranhc7:Manjukiran@cluster0.56lwvzb.mongodb.net/?retryWrites=true&w=majority')
@@ -20,8 +22,21 @@ app.post('/register', async (req,res) => {
         res.json(userDoc);
     } catch(e) {
         res.status(400).json(e);
-    }
-    
+    }    
 });
+app.post('/login', async(req,res) => {
+    console.log(req.body)
+    const{username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = await bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(403).json('Invalid username and password entered')
+    }
+})  
 
 app.listen(4000);
